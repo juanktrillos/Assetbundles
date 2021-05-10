e14790992e562be8b0c3bbfc47cacaeaa18c562b
juanktrillos <juanktrillos@gmail.com>
2021-05-10 11:53:00 -0500

    update

---
+++ Assets/WebGLBuilder.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+class WebGLBuilder {
+    static void build () {
+
+        // Place all your scenes here
+        string[] scenes = {
+            "Assets/Scenes/Escena0.unity"
+        };
+
+        string pathToDeploy = "Build/WebGLversion/";
+
+        BuildPipeline.BuildPlayer (scenes, pathToDeploy, BuildTarget.WebGL, BuildOptions.None);
+    }
+}
